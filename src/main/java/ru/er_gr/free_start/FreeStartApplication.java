47c62f2e7dc5c5b16917547c0e3a5e583e0b3755
ruarsv5 <Vadim.Ershov@raiffeisen.ru>
2022-01-10 00:31:22 +0300

    init

---
+++ src/main/java/ru/er_gr/free_start/FreeStartApplication.java
@@ -0,0 +1,13 @@
+package ru.er_gr.free_start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeStartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/er_gr/free_start/FreeStartApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.er_gr.free_start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
